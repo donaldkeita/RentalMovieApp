6cc3868db46cfc630e5e68a1f9bfba8b05e48269
DonaldKeita <keitadonald@hotmail.com>
2022-07-18 11:03:32 -0400

    updated Abby app

--- Abby/AbbyWeb/Data/ApplicationDbContext.cs
+++ Abby/AbbyWeb/Data/ApplicationDbContext.cs
@@ -5,10 +5,13 @@ namespace AbbyWeb.Data
 {
     public class ApplicationDbContext : DbContext
     {
+
+        public DbSet<Category> Category { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
         }
-        public DbSet<Category> Category { get; set; }
+        
     }
 }
--- Abby/AbbyWeb/Pages/Categories/Create.cshtml.cs
+++ Abby/AbbyWeb/Pages/Categories/Create.cshtml.cs
@@ -1,13 +1,47 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using AbbyWeb.Model;
+using AbbyWeb.Data;
 
 
 namespace AbbyWeb.Pages.Categories
 {
+
+    [BindProperties]
+
     public class CreateModel : PageModel
     {
+
+        private readonly ApplicationDbContext _db;
+
+        public Category Category { get; set; }
+
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+                
+        }
+
+
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (Category.Name == Category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError(string.Empty, "The value entered in DisplayOrder cannot exactly match the value in Name");
+            }
+
+            if(ModelState.IsValid)
+            {
+                await _db.Category.AddAsync(Category);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
         }
     }
 }
